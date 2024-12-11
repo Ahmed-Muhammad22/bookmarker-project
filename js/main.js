a3fcf9d0187bb667bfa063094006d6fa1e06aa2a
Ahmed-Muhammad22 <ahmed.khater22698@gmail.com>
2024-12-11 19:56:16 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
-var siteNameInput = document.getElementById("book-name"); 
-var siteUrlInput = document.getElementById("book-url");
+var siteName = document.getElementById("siteName");
+var siteUrl = document.getElementById("siteUrl");
 var closeBtn = document.getElementById("closeBtn");
 var message = document.getElementById("message");
 var box = document.getElementById("box");
 var bookMarkList = [];
 if (localStorage.getItem("bookMarkContainer") !== null) {
-    bookMarkList = JSON.parse(localStorage.getItem("bookMarkContainer"));
-    displayData();
+  bookMarkList = JSON.parse(localStorage.getItem("bookMarkContainer"));
+  displayData();
 }
 function addData() {
-    if (validationName() && validationUrl()) {
-        var bookMark = {
-            siteName: siteNameInput.value,
-            siteUrl: siteUrlInput.value,
-        };
-        bookMarkList.push(bookMark);
-        localStorage.setItem("bookMarkContainer", JSON.stringify(bookMarkList));
-        console.log(bookMarkList);
-        displayData();
-        clearData();
-    }
-    else {
-        message.classList.remove("d-none");
-        box.classList.remove("d-none")
-       document.addEventListener("click", function (e) {
-                if (e.target == box) {
-                    box.classList.add("d-none");
-                   }
-                 });
-    }
+  if (allValidation(siteName, "msgName") && allValidation(siteUrl, "msgUrl")) {
+    var bookMark = {
+      name: siteName.value,
+      url: siteUrl.value,
+    };
+    bookMarkList.push(bookMark);
+    localStorage.setItem("bookMarkContainer", JSON.stringify(bookMarkList));
+    console.log(bookMarkList);
+    displayData();
+    clearData();
+  } else {
+    message.classList.remove("d-none");
+    box.classList.remove("d-none");
+    document.addEventListener("click", function (e) {
+      if (e.target == box) {
+        box.classList.add("d-none");
+      }
+    });
+  }
 }
 function displayData() {
-    var cartoona = "";
-    for (let i = 0; i < bookMarkList.length; i++) {
-        cartoona += `
+  var cartoona = "";
+  for (let i = 0; i < bookMarkList.length; i++) {
+    cartoona += `
              <tr>
                 <td>
                     ${i + 1}
                 </td>
                 <td>
-                   ${bookMarkList[i].siteName}
+                   ${bookMarkList[i].name}
                 </td>
                 <td>
-                    <button onclick="visitData(${i})" class="btn site-visit pe-2">
+                    <button onclick="window.open('${
+                      bookMarkList[i].url}','_blank')" class="btn site-visit pe-2">
                         <i class="fa-regular fa-eye pe-1"></i>
                         Visit
                     </button>
@@ -55,64 +55,43 @@ function displayData() {
                 </td>
                </tr>
         `;
-        
-    }
-    document.getElementById("tbody").innerHTML = cartoona;
+  }
+  document.getElementById("tbody").innerHTML = cartoona;
 }
 
 function clearData() {
-  siteNameInput.value = "";
-  siteUrlInput.value = "";
-  siteNameInput.classList.remove("is-valid");
-  siteUrlInput.classList.remove("is-valid");
-}
-
-function visitData(index) {
-    var website= bookMarkList[index].siteUrl;
-    var regex =
-      /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)$/gi;
-
-    if (regex.test(website)) {
-        window.open(website, "_blank")
-    }
+  siteName.value = "";
+  siteUrl.value = "";
+  siteName.classList.remove("is-valid", "is-invalid");
+  siteUrl.classList.remove("is-valid", "is-invalid");
 }
 
 function deleteData(index) {
-    bookMarkList.splice(index, 1);
-    localStorage.setItem("bookMarkContainer", JSON.stringify(bookMarkList));
-    displayData();
+  bookMarkList.splice(index, 1);
+  localStorage.setItem("bookMarkContainer", JSON.stringify(bookMarkList));
+  displayData();
 }
 
-function validationName() {
-    var text = siteNameInput.value;
-    var regex = /^\w{3,}(\s+\w+)*$/;
-    if (regex.test(text)) {
-        siteNameInput.classList.add("is-valid");
-        siteNameInput.classList.remove("is-invalid");
-        return true;
-    }
-    else {
-         siteNameInput.classList.add("is-invalid");
-         siteNameInput.classList.remove("is-valid");
-        return false;
-    }
+function allValidation(element, msgId) {
+  let msg = document.getElementById(msgId);
+
+  var regex = {
+    siteName: /^\w{3,}(\s+\w+)*$/,
+    siteUrl:
+      /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_+.~#?&//=]*)$/gi,
+  };
+  if (regex[element.id].test(element.value)) {
+    element.classList.add("is-valid");
+    element.classList.remove("is-invalid");
+    msg.classList.add("d-none");
+    return true;
+  } else {
+    element.classList.add("is-invalid");
+    element.classList.remove("is-valid");
+    msg.classList.remove("d-none");
+    return false;
+  }
 }
-
-function validationUrl() {
-    var url = siteUrlInput.value;
-    var regex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)$/gi;
-
-    if (regex.test(url)) {
-        siteUrlInput.classList.add("is-valid");
-        siteUrlInput.classList.remove("is-invalid");
-        return true;
-    } else {
-        siteUrlInput.classList.remove("is-valid");
-        siteUrlInput.classList.add("is-invalid");
-        return false;
-    }
-}
-
 closeBtn.addEventListener("click", function () {
-    box.classList.add("d-none");
+  box.classList.add("d-none");
 });
